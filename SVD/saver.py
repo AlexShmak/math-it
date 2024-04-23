e3c268c1346c8d6390a81a113e4501be8599218a
alexander <lexnder9@gmail.com>
2024-04-23 23:23:34 +0300

    fix: compression strategy

--- SVD/saver.py
+++ SVD/saver.py
@@ -34,7 +34,7 @@ def write_matrices(path, red, green, blue):
 
 
 def file_size(path):
-    return str((os.path.getsize(path) // 1024)) + " Kbs"
+    return str((os.path.getsize(path) / 1024)) + " Kbs"
 
 
 def read_matrices(path):
